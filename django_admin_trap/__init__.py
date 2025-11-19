3ae6287c1626362ae28207683fb4742a12dd85a0
Jamil Ahmed <jamilcodes@gmail.com>
2025-11-19 12:44:39 +0500

    Version 1.1.1 -> fixed homepage url on pypi page

--- django_admin_trap/__init__.py
+++ django_admin_trap/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Jamil Ahmed"
 __email__ = "contact@jamilcodes.com"
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="django-admin-trap",
-    version="1.1.0",
+    version="1.1.1",
     author="Jamil Ahmed",
     author_email="contact@jamilcodes.com",
     description="A completely fake Django admin login page - perfect honeypot",
